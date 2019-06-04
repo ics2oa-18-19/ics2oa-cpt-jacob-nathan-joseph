5b1a0b1d246565e5f3d7d04a4564fb888dfa2add
nlaids <47423885+nlaids@users.noreply.github.com>
2019-06-04 10:14:00 -0400

    Update cpt.py

--- cpt.py
+++ cpt.py
@@ -1,1 +1,50 @@
-hello
+import os
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+player = arcade.Sprite('Sprites/Shaggy.png', center_x=WIDTH/2, center_y=HEIGHT/2, scale=0.5)
+
+
+
+
+def setup():
+    arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1/60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+    window.on_mouse_press = on_mouse_press
+
+    arcade.run()
+
+
+def update(delta_time):
+    pass
+
+
+def on_draw():
+    arcade.start_render()
+    player.draw()
+
+def on_key_press(key, modifiers):
+    pass
+
+
+def on_key_release(key, modifiers):
+    pass
+
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+if __name__ == '__main__':
+    setup()
