1ee0f8116e31e35280bbd175e893bdf1e6ae038e
Josephxie <47423895+Josephxie@users.noreply.github.com>
2019-06-16 20:46:50 -0400

    Update sans_moves.py

--- sans_moves.py
+++ sans_moves.py
@@ -1,64 +1,58 @@
 import arcade
 import random
+
 WIDTH = 640
 HEIGHT = 480
 
-player_x = WIDTH/2
-player_y = HEIGHT/2
+player_x = WIDTH / 2
+player_y = HEIGHT / 2
 
 up_pressed = False
 down_pressed = False
 left_pressed = False
 right_pressed = False
 
-player = arcade.Sprite('images/shagstill.png', center_x=WIDTH/2, center_y=350, scale=0.1)
+player = arcade.Sprite('shag.png', center_x=WIDTH / 2, center_y=350, scale=0.1)
 
-#random coordinate variables
-Herobrine_move = random.randint(1,3)
-location_1 = random.randint(1,640)
-location_2 = random.randint(1,640)
-location_3 = random.randint(1,640)
-location_4 = random.randint(1,640)
-location_5 = random.randint(1,640)
-location_x_1 = random.randint(240,480)
-location_x_2 = random.randint(240,480)
-location_x_3 = random.randint(240,480)
-location_x_4 = random.randint(240,480)
-location_x_5 = random.randint(240,480)
+# random coordinate variables
+sans_move = random.randint(1, 3)
+location_1 = random.randint(1, 640)
+location_2 = random.randint(1, 640)
+location_3 = random.randint(1, 640)
+location_4 = random.randint(1, 640)
+location_5 = random.randint(1, 640)
+location_x_1 = random.randint(240, 480)
+location_x_2 = random.randint(240, 480)
+location_x_3 = random.randint(240, 480)
+location_x_4 = random.randint(240, 480)
+location_x_5 = random.randint(240, 480)
 
-#attacks
-cock = arcade.Sprite('images/sans_cock.png', center_x = 40, center_y = random.randint(265,450), scale=0.2)
-cock_2 = arcade.Sprite('images/sans_cock.png', center_x = 600, center_y = random.randint(265,450), scale=0.2)
+# attacks
+cock = arcade.Sprite('sans_cock.png', center_x=40, center_y=random.randint(265, 450), scale=0.2)
+cock_2 = arcade.Sprite('sans_cock.png', center_x=600, center_y=random.randint(265, 450), scale=0.2)
 cock.change_x = +8
 cock_2.change_x = -8
 
-bone = arcade.Sprite('images/sans_bone.png', center_x = 700, center_y = location_x_1, scale=0.35)
-bone_2 = arcade.Sprite('images/sans_bone.png', center_x = 700, center_y = location_x_2, scale=0.35)
-bone_3 = arcade.Sprite('images/sans_bone2.png', center_x = location_1, center_y = 690, scale = 0.35)
-bone_4 = arcade.Sprite('images/sans_bone2.png', center_x = location_2, center_y = 690, scale = 0.35)
+bone = arcade.Sprite('sans_bone.png', center_x=700, center_y=location_x_1, scale=0.35)
 bone.change_x = -10
-bone_2.change_x = -10
-bone_3.change_y = -10
-bone_4.change_y = -10
 
-
-emeralds = arcade.Sprite('images/sans_master_emerald.png', center_x=700, center_y =location_x_1, scale=0.2)
-emeralds_2 = arcade.Sprite('images/sans_blue_emerald.png', center_x= 690, center_y = location_x_2, scale= 0.07)
-emeralds_3 = arcade.Sprite('images/sans_yellow_emerald.png', center_x= 750, center_y = location_x_3, scale= 0.4)
-emeralds_4 = arcade.Sprite('images/sans_red_emerald.png', center_x= 800, center_y = location_x_4, scale= 0.08)
-emeralds_5 = arcade.Sprite('images/sans_white_emerald.png', center_x= 840, center_y = location_x_5, scale= 0.07)
+emeralds = arcade.Sprite('sans_master_emerald.png', center_x=700, center_y=location_x_1, scale=0.2)
+emeralds_2 = arcade.Sprite('sans_blue_emerald.png', center_x=690, center_y=location_x_2, scale=0.07)
+emeralds_3 = arcade.Sprite('sans_yellow_emerald.png', center_x=750, center_y=location_x_3, scale=0.4)
+emeralds_4 = arcade.Sprite('sans_red_emerald.png', center_x=800, center_y=location_x_4, scale=0.08)
+emeralds_5 = arcade.Sprite('sans_white_emerald.png', center_x=840, center_y=location_x_5, scale=0.07)
 emeralds.change_x = -10
 emeralds_2.change_x = -10
 emeralds_3.change_x = -10
 emeralds_4.change_x = -10
 emeralds_5.change_x = -10
 
-button1 = [22, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
-button2 = [184, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
-button3 = [353, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
-button4 = [518, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
+button6 = [22, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
+button7 = [184, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
+button8 = [353, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
+button9 = [518, 1.5, 115, 40, False, (255, 0, 0, 0), arcade.color.GREEN]
 
-BTN_X = 1
+BTN_X = 0
 BTN_Y = 1
 BTN_WIDTH = 2
 BTN_HEIGHT = 3
@@ -67,14 +61,17 @@ BTN_COLOR = 5
 BTN_CLICKED_COLOR = 6
 
 health = 100
+sans_health = 150
 
-fight = arcade.Sprite('images/screen_fight.png', center_x=WIDTH/2, center_y=HEIGHT/2, scale=1)
+n = 1
+
+fight = arcade.Sprite('fight.png', center_x=WIDTH / 2, center_y=HEIGHT / 2, scale=1)
 
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
     arcade.set_background_color(arcade.color.BLACK)
-    arcade.schedule(update, 1/60)
+    arcade.schedule(update, 1 / 60)
 
     # Override arcade window methods
     window = arcade.get_window()
@@ -89,53 +86,80 @@ def on_draw():
     arcade.start_render()
     fight.draw()
     player.draw()
-    if Herobrine_move == 1:
-            cock.draw()
-            cock_2.draw()
-    if Herobrine_move == 2:
-            bone.draw()
-            bone_2.draw()
-            bone_3.draw()
-            bone_4.draw()
-    if Herobrine_move == 3:
-            emeralds.draw()
-            emeralds_2.draw()
-            emeralds_3.draw()
-            emeralds_4.draw()
-            emeralds_5.draw()
+    cock.draw()
+    cock_2.draw()
+    bone.draw()
+    emeralds.draw()
+    emeralds_2.draw()
+    emeralds_3.draw()
+    emeralds_4.draw()
+    emeralds_5.draw()
+    if cock.center_x == 600:
+        cock.change_x = -10
+        cock.center_y = random.randint(265,450)
+    elif cock.center_x == 40:
+        cock.change_x = 10
+        cock.center_y = random.randint(265, 450)
+    if cock_2.center_x == 40:
+        cock_2.change_x = 10
+        cock_2.center_y = random.randint(265, 450)
+    elif cock_2.center_x == 600:
+        cock_2.change_x = -10
+        cock_2.center_y = random.randint(265, 450)
+    if bone.center_y == 400:
+        bone.center_y += HEIGHT
+        bone.center_x = random.randint(1,640)
+    if emeralds.center_y == 160:
+        emeralds.center_y += HEIGHT
+        emeralds.center_x = random.randint(1,640)
+    if emeralds_2.center_y == 160:
+        emeralds_2.center_y += HEIGHT
+        emeralds_2.center_x = random.randint(1,640)
+    if emeralds_3.center_y == 160:
+        emeralds_3.center_y += HEIGHT
+        emeralds_3.center_x = random.randint(1,640)
+    if emeralds_4.center_y == 160:
+        emeralds_4.center_y += HEIGHT
+        emeralds_4.center_x = random.randint(1,640)
+    if emeralds_5.center_y == 160:
+        emeralds_5.center_y += HEIGHT
+        emeralds_5.center_x = random.randint(1,640)
 
+    if button6[BTN_IS_CLICKED]:
+        color = button6[BTN_CLICKED_COLOR]
+    else:
+        color = button6[BTN_COLOR]
+    arcade.draw_xywh_rectangle_filled(button6[BTN_X], button6[BTN_Y], button6[BTN_WIDTH], button6[BTN_HEIGHT],
+                                      color)
 
+    if button7[BTN_IS_CLICKED]:
+        color = button7[BTN_CLICKED_COLOR]
+    else:
+        color = button7[BTN_COLOR]
+    arcade.draw_xywh_rectangle_filled(button7[BTN_X], button7[BTN_Y], button7[BTN_WIDTH], button7[BTN_HEIGHT],
+                                      color)
+
+    if button8[BTN_IS_CLICKED]:
+        color = button8[BTN_CLICKED_COLOR]
+    else:
+        color = button8[BTN_COLOR]
+    arcade.draw_xywh_rectangle_filled(button8[BTN_X], button8[BTN_Y], button8[BTN_WIDTH], button8[BTN_HEIGHT],
+                                      color)
+
+    if button9[BTN_IS_CLICKED]:
+        color = button9[BTN_CLICKED_COLOR]
+    else:
+        color = button9[BTN_COLOR]
+    arcade.draw_xywh_rectangle_filled(button9[BTN_X], button9[BTN_Y], button9[BTN_WIDTH], button9[BTN_HEIGHT],
+                                      color)
 
     arcade.draw_xywh_rectangle_filled(220, 50, health * 2, 20, arcade.color.GREEN)
-    if button1[BTN_IS_CLICKED]:
-        color = button1[BTN_CLICKED_COLOR]
-    else:
-        color = button1[BTN_COLOR]
-    arcade.draw_xywh_rectangle_filled(button1[BTN_Y > 184], button1[BTN_X], button1[BTN_WIDTH], button1[BTN_HEIGHT],
-                                      color)
-    if button2[BTN_IS_CLICKED]:
-        color = button2[BTN_CLICKED_COLOR]
-    else:
-        color = button2[BTN_COLOR]
-    arcade.draw_xywh_rectangle_filled(button2[BTN_Y > 184], button2[BTN_X], button2[BTN_WIDTH], button2[BTN_HEIGHT],
-                                      color)
-    if button3[BTN_IS_CLICKED]:
-        color = button3[BTN_CLICKED_COLOR]
-    else:
-        color = button3[BTN_COLOR]
-    arcade.draw_xywh_rectangle_filled(button3[BTN_Y > 184], button3[BTN_X], button3[BTN_WIDTH], button3[BTN_HEIGHT],
-                                      color)
-    if button4[BTN_IS_CLICKED]:
-        color = button4[BTN_CLICKED_COLOR]
-    else:
-        color = button4[BTN_COLOR]
-    arcade.draw_xywh_rectangle_filled(button4[BTN_Y > 184], button4[BTN_X], button4[BTN_WIDTH], button4[BTN_HEIGHT],
-                                      color)
+    arcade.draw_xywh_rectangle_filled(160, 90, sans_health * 2, 20, arcade.color.RED)
 
 
 def update(delta_time):
-#moving shaggy
-    global up_pressed, player_y
+    # moving shaggy
+    global up_pressed, player_y, health, sans_health
     if up_pressed == True:
         player.center_y += 6
     if down_pressed == True:
@@ -145,17 +169,7 @@ def update(delta_time):
     if right_pressed == True:
         player.center_x += 6
 
-#border
-    if player.center_x > 640:
-        player.center_x -= 5
-    if player.center_x < 20:
-        player.center_x += 5
-    if player.center_y > 480:
-        player.center_y -= 5
-    if player.center_y < 230:
-        player.center_y += 5
-    
-    #stopping attacks
+    # stopping attacks
     cock.update()
     cock_2.update()
     bone.update()
@@ -186,9 +200,56 @@ def update(delta_time):
     if emeralds_5.center_y == 265:
         emeralds_5.stop()
 
+    if button6[BTN_IS_CLICKED]:
+        sans_health -= 1
+
+    if button7[BTN_IS_CLICKED]:
+        health += 0.7
+
+    cock.update()
+
+    cock_hit = arcade.check_for_collision(player, cock)
+    while cock_hit == True:
+        health -= 2
+        break
+    cock_2.update()
+    cock_hit_2 = arcade.check_for_collision(player, cock_2)
+    while cock_hit_2 == True:
+        health -= 2
+        break
+
+    bone.update()
+    bone_hit = arcade.check_for_collision(player, bone)
+    while bone_hit == True:
+        health -= 1
+        break
+    emeralds.update()
+    hit = arcade.check_for_collision(player, emeralds)
+    while hit == True:
+        health -= 2
+        break
+    emeralds_2.update()
+    hit_2 = arcade.check_for_collision(player, emeralds_2)
+    while hit_2 == True:
+        health -= 2
+        break
+    emeralds_3.update()
+    hit_3 = arcade.check_for_collision(player, emeralds_3)
+    while hit_3 == True:
+        health -= 2
+        break
+    emeralds_4.update()
+    hit_4 = arcade.check_for_collision(player, emeralds_4)
+    while hit_4 == True:
+        break
+    emeralds_5.update()
+    hit_5 = arcade.check_for_collision(player, emeralds_5)
+    while hit_5 == True:
+        health -= 2
+        break
 
 def on_key_press(key, modifiers):
-#key presses
+    # key presses
     global up_pressed
     if key == arcade.key.W:
         up_pressed = True
@@ -223,34 +284,33 @@ def on_key_release(key, modifiers):
 
 
 def on_mouse_press(x, y, button, modifiers):
-#buttons
+    # buttons
     print(f"Click at ({x}, {y})")
-    if (x > button1[BTN_X] and x < button1[BTN_X] + button1[BTN_WIDTH] and
-            y > button1[BTN_Y] and y < button1[BTN_Y] + button1[BTN_HEIGHT]):
-        button1[BTN_IS_CLICKED] = True
-    if (x > button2[BTN_X] and x < button2[BTN_X] + button2[BTN_WIDTH] and
-            y > button2[BTN_Y] and y < button2[BTN_Y] + button2[BTN_HEIGHT]):
-        button2[BTN_IS_CLICKED] = True
-    if (x > button3[BTN_X] and x < button3[BTN_X] + button3[BTN_WIDTH] and
-                y > button3[BTN_Y] and y < button3[BTN_Y] + button3[BTN_HEIGHT]):
-            button3[BTN_IS_CLICKED] = True
-    if (x > button4[BTN_X] and x < button4[BTN_X] + button4[BTN_WIDTH] and
-                y > button4[BTN_Y] and y < button4[BTN_Y] + button4[BTN_HEIGHT]):
-            button4[BTN_IS_CLICKED] = True
+    if (x > button6[BTN_X] and x < button6[BTN_X] + button6[BTN_WIDTH] and
+            y > button6[BTN_Y] and y < button6[BTN_Y] + button6[BTN_HEIGHT]):
+        button6[BTN_IS_CLICKED] = True
+    if (x > button7[BTN_X] and x < button7[BTN_X] + button7[BTN_WIDTH] and
+            y > button7[BTN_Y] and y < button7[BTN_Y] + button7[BTN_HEIGHT]):
+        button7[BTN_IS_CLICKED] = True
+    if (x > button8[BTN_X] and x < button8[BTN_X] + button8[BTN_WIDTH] and
+            y > button8[BTN_Y] and y < button8[BTN_Y] + button8[BTN_HEIGHT]):
+        button8[BTN_IS_CLICKED] = True
+    if (x > button9[BTN_X] and x < button9[BTN_X] + button9[BTN_WIDTH] and
+            y > button9[BTN_Y] and y < button9[BTN_Y] + button9[BTN_HEIGHT]):
+        button9[BTN_IS_CLICKED] = True
 
 
 def on_mouse_release(x, y, button, modifiers):
-
-    button1[BTN_IS_CLICKED] = False
-    button2[BTN_IS_CLICKED] = False
-    button3[BTN_IS_CLICKED] = False
-    button4[BTN_IS_CLICKED] = False
+    button6[BTN_IS_CLICKED] = False
+    button7[BTN_IS_CLICKED] = False
+    button8[BTN_IS_CLICKED] = False
+    button9[BTN_IS_CLICKED] = False
 
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
     arcade.set_background_color(arcade.color.WHITE)
-    arcade.schedule(update, 1/60)
+    arcade.schedule(update, 1 / 60)
 
     # Override arcade window methods
     window = arcade.get_window()
